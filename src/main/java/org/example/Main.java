e7c823ae763d7cf28fa6eade2604f507c6c2b0de
alvaropallares20 <alvaropallaresm@gmail.com>
2022-10-10 12:22:12 +0200

    AGREGAR, BORRAR UN ELEMENTO O BORRAR ENTERA LA LISTA

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -125,7 +125,7 @@ public class Main {
             }
         });
 
-        JButton deleteAll = new JButton("BorrRar Lista");
+        JButton deleteAll = new JButton("BorrRRar Lista");
         mainPanel.add(
                 deleteAll,
                 new GridBagConstraints(
